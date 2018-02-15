206099ac38cb40e9adffd08ec54908b4da977807
AAngeloB <asvpangelob@gmail.com>
2018-02-14 14:33:35 -1000

    later

---
+++ app.js
@@ -0,0 +1,12 @@
+var fortunesList= [
+	"you'll lose a hand",
+	"you'll lose a foot",
+	"something unexpected is coming",
+	"the last fortunue was a lie",
+	"the next fortune is a lie"
+];
+console.log(fortunesList[4]);
+
+for (i = 0; i < fortunesList; i++){
+	return fortunesList[0];
+}
